9087f66e583978b4d5bbf2f4714fa9e1909112e6
noro6 <noro_next@outlook.jp>
2019-11-30 14:39:14 +0900

    E-4一部　ネ級改仮置き

--- js/data.js
+++ js/data.js
@@ -608,6 +608,7 @@ const ENEMY_DATA = [
   { id: 392, type: [11, 10, 1000, 1908], name: "飛行場姫 重爆 弱", slot: [18, 12, 6], aa: [9, 9, 9], orig: 56, isSpR: false },
   { id: 393, type: [11, 10, 1000, 1908], name: "飛行場姫 重爆 中", slot: [18, 12, 12, 6], aa: [17, 9, 9, 9], orig: 56, isSpR: false },
   { id: 394, type: [11, 10, 1000, 1908], name: "飛行場姫 重爆 強", slot: [36, 18, 12, 6], aa: [17, 17, 17, 17], orig: 56, isSpR: false },
+  { id: 400, type: [4], name: "重巡ネ級改？", slot: [4], aa: [1], orig: 94, isSpR: true },
 ];
 
 // 難易度
@@ -672,7 +673,7 @@ const MAP_DATA = [
   { area: 19111, name: 'マカッサル沖/バリ島沖' },
   { area: 19112, name: 'ジャワ沖/ダーウィン沖' },
   { area: 19113, name: 'ジャワ海/スラバヤ沖' },
-  { area: 19114, name: 'イベント海域4(仮設置)' },
+  { area: 19114, name: 'バタビア沖' },
   { area: 19115, name: 'イベント海域5(仮設置)' },
   { area: 19116, name: 'イベント海域6(仮設置)' }
 ];
@@ -975,12 +976,15 @@ const ENEMY_PATTERN = [
   { area: 19113, name: 'I', enemies: [280, 27, 27, 92], range: 4, difficulty: 3 },
   { area: 19113, name: 'G', enemies: [27, 27, 92], range: 3, difficulty: 3 },
   { area: 19113, name: 'J', enemies: [55, 27, 27, 92], range: 4, difficulty: 3 },
-  { area: 19113, name: 'Mボス 前哨', enemies: [92], range: 5, difficulty: 3 },
-  { area: 19113, name: 'Mボス 最終', enemies: [92], range: 5, difficulty: 3 },
+  { area: 19113, name: 'Mボス 前哨', enemies: [400, 92], range: 5, difficulty: 3 },
+  { area: 19113, name: 'Mボス 最終', enemies: [400, 92], range: 5, difficulty: 3 },
   { area: 19113, name: 'Oボス 前哨', enemies: [104, 27, 276], range: 6, difficulty: 3 },
   { area: 19113, name: 'Oボス 最終', enemies: [295, 27, 266, 92], range: 6, difficulty: 3 },
 
-  { area: 19114, name: '', enemies: [], range: 0, difficulty: 3 },
+  { area: 19114, name: '空襲', enemies: [394, 394, 150, 150], range: 0, difficulty: 3 },
+  { area: 19114, name: '空襲', enemies: [394, 150, 150], range: 0, difficulty: 3 },
+  { area: 19114, name: 'M', enemies: [27, 92], range: 0, difficulty: 3 },
+  { area: 19114, name: 'Pボス', enemies: [400, 92], range: 0, difficulty: 3 },
 ];
 
 /** デフォ機体プリセット */
