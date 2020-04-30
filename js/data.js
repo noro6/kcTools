956320e0006c8520055f9e8f3289e18dfb401d62
noro6 <noro_next@outlook.jp>
2020-04-30 13:03:06 +0900

    初期画面修正

--- js/data.js
+++ js/data.js
@@ -2261,6 +2261,10 @@ let CHANGE_LOG = [
         content: "入力した敵艦隊毎に、敵艦それぞれの割合、固定撃墜数などを一覧で表示します。同画面内にて簡易的な撃墜数の試算も可能です。敵艦隊欄の <i class='fas fa-exclamation-circle text-primary'></i> アイコンをクリックすると起動します。"
       },
       {
+        type: 1, title: "共有URL発行時のURLを短縮化するように変更しました。",
+        content: ""
+      },
+      {
         type: 2, title: "不具合の修正を行いました。",
         content: "敵艦隊未設定時に計算結果欄の表示が意図していない表示になっていた現象を修正しました"
       },
@@ -2268,4 +2272,4 @@ let CHANGE_LOG = [
   },
 ];
 
-const LAST_UPDATE_DATE = "2020/04/28";
+const LAST_UPDATE_DATE = "2020/04/30";
