22f975c5245f734094358219c5c2b56e97053d73
noro6 <noro_next@outlook.jp>
2021-01-25 22:08:14 +0900

    基地派遣戦闘(表示時)の補給処理修正 加賀改二護に景雲が詰めた問題を修正

--- js/data.js
+++ js/data.js
@@ -1317,8 +1317,8 @@ const SPECIAL_LINK_SHIP_ITEM = [
 	// 翔鶴型改二甲に 噴式機
 	{ shipId: 266, itemTypes: [57], itemIDs: [151] },
 	{ shipId: 267, itemTypes: [57], itemIDs: [151] },
-	// 加賀改二戊に 噴式機
-	{ shipId: 446, itemTypes: [57], itemIDs: [151] },
+	// 加賀改二護に 噴式機
+	{ shipId: 446, itemTypes: [57], itemIDs: [] },
 	// 伊勢型改二に 艦戦 艦爆 艦偵
 	{ shipId: 353, itemTypes: [6, 7, 9], itemIDs: [] },
 	{ shipId: 354, itemTypes: [6, 7, 9], itemIDs: [] },
